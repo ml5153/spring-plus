ede61947fc34b92042ce3151a4f7ccabbc837789
yspark <youngsue5153@gmail.com>
2026-04-03 23:35:18 +0900

    # feat: Spring Security 추가

---
+++ src/main/java/org/example/expert/config/JwtSecurityFilter.java
@@ -0,0 +1,73 @@
+package org.example.expert.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtSecurityFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String token = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(token)) {
+            try {
+                String jwt = jwtUtil.substringToken(token);
+                Claims claims = jwtUtil.extractClaims(jwt);
+
+                Long userId = Long.parseLong(claims.getSubject());
+                String email = claims.get("email", String.class);
+                UserRole userRole = UserRole.valueOf(claims.get("userRole", String.class));
+
+                // AuthUser에서는 nickname이 필요없을 듯하여 null로 처리
+                AuthUser authUser = new AuthUser(userId, email, userRole, null);
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(authUser, null, authUser.getAuthorities());
+
+                // SecurityContextHolder 에 저장해야 인증된 사용자라고 인식함.
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } catch (SecurityException | MalformedJwtException e) {
+                log.error("Invalid JWT signature", e);
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
+                return;
+            } catch (ExpiredJwtException e) {
+                log.error("Expired JWT token", e);
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "만료된 JWT 토큰입니다.");
+                return;
+            } catch (UnsupportedJwtException e) {
+                log.error("Unsupported JWT token", e);
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "지원되지 않는 JWT 토큰입니다.");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -2,6 +2,11 @@ package org.example.expert.domain.common.dto;
 
 import lombok.Getter;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 public class AuthUser {
@@ -17,4 +22,10 @@ public class AuthUser {
         this.userRole = userRole;
         this.nickname = nickname;
     }
+
+    // 권한이 여러 개일 수 있어서 컬렉션으로 반환하게끔 정의되어있다.
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+
 }
